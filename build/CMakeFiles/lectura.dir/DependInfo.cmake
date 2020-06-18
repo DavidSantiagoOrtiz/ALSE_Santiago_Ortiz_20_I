898d90e1569ab00b8c0d8790b917a81edbd7ce22
DavidSantiagoOrtiz <sadin9809@gmail.com>
2020-06-18 09:42:53 -0500

    commit 9:41 18/06/20

---
+++ build/CMakeFiles/lectura.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/lectura.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/build/CMakeFiles/lectura.dir/lectura.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/Circulo.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/Circulo.cpp.o"
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/Cuadrado.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/Cuadrado.cpp.o"
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/Geometrica.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/Geometrica.cpp.o"
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/Pentagono.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/Pentagono.cpp.o"
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/Triangulo.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/Triangulo.cpp.o"
+  "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/main.cpp" "/home/alse-i/Documentos/ALSE_Santiago_Ortiz_20_I/ex4/fig_geometricas_her/build/CMakeFiles/fig_geometricas_her.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
