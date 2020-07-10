04106e2b9f4777d7ef7ee3727c942f9c8b1a062d
alse-a <andres.arias-d@mail.escuelaing.edu.co>
2020-07-10 02:34:09 +0000

    Segundo avance

--- DB_local/DB_local.h
+++ DB_local/DB_local.h
@@ -7,7 +7,10 @@
 #define _DB_LOCAL_H
 
 #include <iostream>
-#include <SQlite3>
+#include <sqlite3.h>
+#include <string>
+#include <stdlib.h>
+#include "DB_local.h"
 #include "Dato.h"
 
 
@@ -16,17 +19,19 @@ using namespace std;
 class DB_local{
 
     public:
-        string path;
-
+        static int callback(void *data, int argc, char **argv, char **azColName);
         bool guardar_dato(Dato d, int h, int m);
         bool abrir_DB();
         bool cerrar_DB();
+        bool ejecutar_DB();
         bool crear_DB(char n_DB);
-        bool borrar_dato();
+        bool borrar_dato(Dato d);
         Dato getdato_minuto(int h, int m);
         Dato getdato(int h, int m, int s);
         Dato getPromedio(int h);
+    private:
 
+        string path;
 };
 
 #endif //_DB_LOCAL_H
